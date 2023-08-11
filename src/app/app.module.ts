2316482e96226e11f7a15fe1737ae1ee36a6b7a1
Jake Hardy <62575622+jakehardy95@users.noreply.github.com>
2023-08-10 20:58:18 -1000

    added bootstrap and new dropdown menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropdownMenuComponent } from './dropdown-menu/dropdown-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownMenuComponent
   ],
   imports: [
     BrowserModule,
